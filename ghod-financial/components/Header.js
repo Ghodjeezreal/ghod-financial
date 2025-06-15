1aaac3776001a4efab746bf5f21fe2215a8d6b82
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-15 21:57:40 +0100

    Update Header.js

--- ghod-financial/components/Header.js
+++ ghod-financial/components/Header.js
@@ -90,42 +90,37 @@ export default function Header() {
           </li>
 
           <li className="relative group">
-  {/* CLICKABLE About Us main link */}
   <Link href="/about" legacyBehavior>
-    <a className="hover:text-accent inline-block">
-      About Us ▾
-    </a>
+    <a className="hover:text-accent inline-block">About Us ▾</a>
   </Link>
-
-  {/* Hoverable + sticky dropdown submenu */}
-  <div className="absolute left-0 mt-2 hidden group-hover:block bg-white shadow-xl rounded-lg w-56 z-[9999] py-2">
-    <ul className="flex flex-col">
+  <div
+    className="
+      absolute left-0 mt-1
+      opacity-0 pointer-events-none
+      group-hover:opacity-100 group-hover:pointer-events-auto
+      transition-opacity duration-200
+      bg-white shadow-xl rounded-lg w-56 z-50
+    "
+  >
+    <ul className="divide-y divide-gray-200">
       <li>
         <Link href="/about/vision" legacyBehavior>
-          <a className="block px-4 py-3 hover:bg-gray-100 whitespace-nowrap">
-            Vision & Mission
-          </a>
+          <a className="block px-4 py-3 hover:bg-gray-100">Vision &amp; Mission</a>
         </Link>
       </li>
       <li>
         <Link href="/about/team" legacyBehavior>
-          <a className="block px-4 py-3 hover:bg-gray-100 whitespace-nowrap">
-            Global Team
-          </a>
+          <a className="block px-4 py-3 hover:bg-gray-100">Global Team</a>
         </Link>
       </li>
       <li>
         <Link href="/about/stories" legacyBehavior>
-          <a className="block px-4 py-3 hover:bg-gray-100 whitespace-nowrap">
-            Success Stories
-          </a>
+          <a className="block px-4 py-3 hover:bg-gray-100">Success Stories</a>
         </Link>
       </li>
       <li>
         <Link href="/about/membership" legacyBehavior>
-          <a className="block px-4 py-3 hover:bg-gray-100 whitespace-nowrap border-t border-gray-200">
-            Membership
-          </a>
+          <a className="block px-4 py-3 hover:bg-gray-100">Membership</a>
         </Link>
       </li>
     </ul>
