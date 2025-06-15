df7c80d8410ad7461cccb80d6a39b810ef825b3d
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-15 17:18:54 +0100

    Update Header.js

--- ghod-financial/components/Header.js
+++ ghod-financial/components/Header.js
@@ -22,7 +22,7 @@ export default function Header() {
           {/* Services Dropdown */}
           <li className="relative group">
             <button className="hover:text-accent focus:outline-none">Services ▾</button>
-            <ul className="absolute left-0 top-full mt-6 hidden group-hover:flex flex-col bg-white shadow-xl rounded-lg w-56
+            <ul className="absolute left-0 top-full mt-2 hidden group-hover:flex flex-col bg-white shadow-xl rounded-lg w-56
                opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out transform 
                scale-95 group-hover:scale-100 z-[9999] py-2">
   <li>
