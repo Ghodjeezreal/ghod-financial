11a0b92333461bc0117e38a31b30338d0c4528ed
Fredrick (Ghod) Okojie <160248180+Ghodjeezreal@users.noreply.github.com>
2025-06-15 22:37:38 +0100

    Update Header.js

--- ghod-financial/components/Header.js
+++ ghod-financial/components/Header.js
@@ -73,14 +73,9 @@ export default function Header() {
   </Link>
 
   {/* Dropdown, positioned flush under the link */}
-  <ul
-    className="
-      absolute
-      top-full left-0        /* zero gap */
-      hidden group-hover:block
-      bg-white shadow-xl rounded-lg w-56 z-50
-    "
-  >
+  <ul className="absolute left-0 -mt-1 hidden group-hover:flex flex-col bg-white shadow-xl rounded-lg w-56
+                 opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out transform 
+                 scale-95 group-hover:scale-100 z-[9999] py-2">
     <li>
       <Link href="/about/vision" legacyBehavior>
         <a className="block px-4 py-3 hover:bg-gray-100 whitespace-nowrap">
